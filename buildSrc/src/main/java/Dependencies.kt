81ca0a89e3e0e45d9ae47ad9fac7d3a2ffbfaec1
Quentin Marciset <quentin.marciset@4d.com>
2020-01-14 16:41:35 +0100

    fix dependency issue

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,6 +7,7 @@ object Versions {
 
     val android_gradle_plugin = "3.5.2"
     val artifactory = "4.13.0"
+    val atsl_junit = "1.1.1"
     val constraint_layout = "2.0.0-beta3"
     val design = "1.0.0"
     val junit = "4.12"
@@ -14,6 +15,12 @@ object Versions {
     val navigation = "2.2.0-rc03"
     val support = "1.1.0"
     val timber = "4.7.1"
+    val robolectric = "4.3.1"
+    val room = "2.2.2"
+    val rx_android = "2.1.1"
+    val rxjava2 = "2.1.3"
+    val retrofit = "2.6.2"
+    val okhttp = "4.2.2"
 }
 
 object Config {
@@ -60,4 +67,22 @@ object Libs {
 
     // Testing
     val junit = "junit:junit:${Versions.junit}"
+
+    // For AndroidMobileAPI
+    val androidx_junit = "androidx.test.ext:junit:${Versions.atsl_junit}"
+    val androidx_preference_ktx = "androidx.preference:preference-ktx:1.1.0"
+    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    val okhttp_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
+    val okhttp_mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.okhttp}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofit_adapter_rxjava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val robolectric = "org.robolectric:robolectric:${Versions.robolectric}"
+    val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rx_android}"
+    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava2}"
+
+    // For AndroidMobileDataStore
+    val androidx_room = "androidx.room:room-ktx:${Versions.room}"
+    val androidx_room_runtime = "androidx.room:room-runtime:${Versions.room}"
+    val androidx_room_compiler = "androidx.room:room-compiler:${Versions.room}"
 }
