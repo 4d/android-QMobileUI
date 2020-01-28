d6886aaf134205394e5ee285c6928551d519bd9a
Quentin Marciset <quentin.marciset@4d.com>
2020-01-28 14:19:21 +0100

    Add comment

--- androidmobileui/src/main/java/com/qmarciset/androidmobileui/utils/NavigationInterface.kt
+++ androidmobileui/src/main/java/com/qmarciset/androidmobileui/utils/NavigationInterface.kt
@@ -2,7 +2,9 @@ package com.qmarciset.androidmobileui.utils
 
 import android.view.View
 
+// Interface implemented by MainActivity to provide different elements depending of the generated type
 interface NavigationInterface {
 
+    // Navigates from ListView to ViewPager (which displays one DetailView)
     fun navigateFromListToViewPager(view: View, position: Int, tableName: String)
 }
