f419ff6badad874d76283e9b70f352462f8e2677
maturi karthik <karthik.maturi@4d.com>
2021-02-11 17:52:02 +0100

    feature searchbar #123701

--- qmobileui/src/main/java/com/qmobile/qmobileui/list/EntityListFragment.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/list/EntityListFragment.kt
@@ -11,7 +11,11 @@ import android.content.Context
 import android.content.Context.SEARCH_SERVICE
 import android.graphics.Color
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -25,7 +29,11 @@ import com.qmobile.qmobileapi.auth.AuthenticationStateEnum
 import com.qmobile.qmobileapi.model.entity.EntityModel
 import com.qmobile.qmobiledatasync.app.BaseApp
 import com.qmobile.qmobiledatasync.sync.DataSyncStateEnum
-import com.qmobile.qmobiledatasync.viewmodel.*
+import com.qmobile.qmobiledatasync.viewmodel.ConnectivityViewModel
+import com.qmobile.qmobiledatasync.viewmodel.EntityListViewModel
+import com.qmobile.qmobiledatasync.viewmodel.LoginViewModel
+import com.qmobile.qmobiledatasync.viewmodel.delete
+import com.qmobile.qmobiledatasync.viewmodel.insert
 import com.qmobile.qmobileui.BaseFragment
 import com.qmobile.qmobileui.FragmentCommunication
 import com.qmobile.qmobileui.R
