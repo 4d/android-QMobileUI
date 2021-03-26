92820467c6fc92c7676432ba807ede69c0a8f066
maturi <karthik.maturi@4d.com>
2021-03-26 08:49:53 +0100

    adding spaces

--- qmobileui/src/main/java/com/qmobile/qmobileui/utils/SqlQueryBuilderUtil.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/utils/SqlQueryBuilderUtil.kt
@@ -33,11 +33,11 @@ class SqlQueryBuilderUtil(
         (0 until columnsToFilter.length()).forEach {
             when {
                 (columnsToFilter.length() == 1) ->
-                    stringBuffer.append("`${columnsToFilter[it]}`LIKE  \'%$dataToSort%\' ")
+                    stringBuffer.append(" `${columnsToFilter[it]}` LIKE  \'%$dataToSort%\' ")
                 (it == (columnsToFilter.length() - 1)) ->
-                    stringBuffer.append("`${columnsToFilter[it]}`LIKE  \'%$dataToSort%\' ")
+                    stringBuffer.append(" `${columnsToFilter[it]}` LIKE  \'%$dataToSort%\' ")
                 else ->
-                    stringBuffer.append("`${columnsToFilter[it]}` LIKE  \'%$dataToSort%\'  OR ")
+                    stringBuffer.append(" `${columnsToFilter[it]}` LIKE  \'%$dataToSort%\'  OR ")
             }
         }
     }
