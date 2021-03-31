351bd9fd627430e4d8b5da799a64a82356221728
Quentin Marciset <quentin.marciset@4d.com>
2021-03-31 16:44:46 +0200

    add sample test

---
+++ qmobileui/src/test/java/com/qmobile/qmobileui/CheckTst.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Maturi Karthik on 31/3/2021.
+ * 4D SAS
+ * Copyright (c) 2021 Maturi Karthik. All rights reserved.
+ */
+
+package com.qmobile.qmobileui
+
+import org.junit.Test
+
+class CheckTst {
+    @Test
+    fun test(){
+        println("test")
+    }
+}
