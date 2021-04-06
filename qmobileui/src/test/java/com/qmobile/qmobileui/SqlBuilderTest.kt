543ce55f9f91187d5a17d13c4f45edeae615a504
maturi <karthik.maturi@4d.com>
2021-04-06 13:45:33 +0200

    Added simple test for searchBar

---
+++ qmobileui/src/test/java/com/qmobile/qmobileui/SqlBuilderTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Created by Maturi Karthik on 2/4/2021.
+ * 4D SAS
+ * Copyright (c) 2021 Maturi Karthik. All rights reserved.
+ */
+
+package com.qmobile.qmobileui
+
+import androidx.sqlite.db.SimpleSQLiteQuery
+import com.qmobile.qmobileui.utils.SqlQueryBuilderUtil
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class SqlBuilderTest {
+
+    private val tableJson: JSONObject = JSONObject().put(
+        "searchableField",
+        mutableListOf(
+            createTable("Employee", listOf("LastName", "LastName")),
+            createTable("Service", listOf("name", "employeeNumber"))
+        )
+    )
+
+    private fun createTable(
+        tableName: String,
+        listOfColumn: List<String>
+    ): HashMap<String, List<String>> {
+        val table = HashMap<String, List<String>>()
+        table[tableName] = listOfColumn
+        return table
+    }
+
+    @Test
+    fun testSqlGetAllQuery() {
+        val sqlQueryBuilder = SqlQueryBuilderUtil(tableName = "Service", searchField = tableJson)
+        val actualQueryResult = sqlQueryBuilder.getAll().sql
+        val expectedQueryResult = SimpleSQLiteQuery("SELECT * FROM Service").sql
+        Assert.assertEquals(expectedQueryResult, actualQueryResult)
+    }
+}
