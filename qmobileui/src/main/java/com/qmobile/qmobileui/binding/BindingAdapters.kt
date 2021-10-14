181133329cd6cd0df70b075af72ad7a842057184
Quentin Marciset <quentin.marciset@4d.com>
2021-10-14 15:22:53 +0200

    add color binding for relation link to be blue

--- qmobileui/src/main/java/com/qmobile/qmobileui/binding/BindingAdapters.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/binding/BindingAdapters.kt
@@ -9,12 +9,15 @@ package com.qmobile.qmobileui.binding
 import android.net.Uri
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.qmobile.qmobileui.R
+import com.qmobile.qmobileui.formatters.FormatterUtils
 import com.qmobile.qmobileui.glide.CustomRequestListener
 
 /**
@@ -82,4 +85,13 @@ fun bindImageFromDrawable(view: ImageView, imageDrawable: Int?) {
 @BindingAdapter("visibleGone")
 fun showHide(view: View, show: Boolean) {
     view.visibility = if (show) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter(
+    value = ["linkColor"]
+)
+fun bindRelationLinkColor(view: TextView, textColor: Boolean?) {
+    if (textColor == true) {
+        view.setTextColor(ContextCompat.getColor(view.context, R.color.relation_link))
+    }
 }
