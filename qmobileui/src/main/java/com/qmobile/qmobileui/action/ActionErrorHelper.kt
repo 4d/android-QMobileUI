9d8d6e643eadcb13aae6f78596d3e307849864ab
htemanni <houssam.temanni@4D.com>
2022-03-24 09:33:49 +0100

    fix build

--- qmobileui/src/main/java/com/qmobile/qmobileui/action/ActionErrorHelper.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/action/ActionErrorHelper.kt
@@ -6,7 +6,8 @@ import java.util.Date
 const val MILLISECONDS_IN_SECONDS = 1000
 fun shouldShowActionError(): Boolean {
     var sharedPreferencesHolder = BaseApp.sharedPreferencesHolder
-    val lastDisplayErrorTime = Date(sharedPreferencesHolder.lastTimeActionErrorDisplayed).time
+    val last = BaseApp.sharedPreferencesHolder.lastTimeActionErrorDisplayed
+    val lastDisplayErrorTime = Date(last).time
     val diffInSeconds = (Date().time - lastDisplayErrorTime) / MILLISECONDS_IN_SECONDS
 
     if (diffInSeconds >= SECONDS_IN_MINUTE) {
