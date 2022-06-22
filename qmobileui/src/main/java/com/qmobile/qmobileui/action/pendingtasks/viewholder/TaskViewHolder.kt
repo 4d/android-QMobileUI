0313aaa203bb40e7057a2d7dc3847c50300eb7b5
htemanni <houssam.temanni@4D.com>
2022-06-22 14:42:40 +0200

    fix nesteddepthblock

--- qmobileui/src/main/java/com/qmobile/qmobileui/action/pendingtasks/viewholder/TaskViewHolder.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/action/pendingtasks/viewholder/TaskViewHolder.kt
@@ -15,25 +15,12 @@ import com.qmobile.qmobiledatastore.dao.ActionTask
 import com.qmobile.qmobiledatasync.app.BaseApp
 import com.qmobile.qmobileui.R
 import com.qmobile.qmobileui.action.actionparameters.ActionParameterEnum
-import com.qmobile.qmobileui.action.actionparameters.viewholder.AM_KEY
-import com.qmobile.qmobileui.action.actionparameters.viewholder.PM_KEY
 import com.qmobile.qmobileui.action.model.Action
 import com.qmobile.qmobileui.action.pendingtasks.TaskListViewHolder
 import com.qmobile.qmobileui.action.utils.ActionHelper
 import com.qmobile.qmobileui.action.utils.DotProgressBar
-import com.qmobile.qmobileui.action.utils.getDayWord
-import com.qmobile.qmobileui.action.utils.getHourWord
-import com.qmobile.qmobileui.action.utils.getMinuteWord
-import com.qmobile.qmobileui.action.utils.getSecondWord
+import com.qmobile.qmobileui.action.utils.DateTimeHelper
 import com.qmobile.qmobileui.formatters.FormatterUtils
-import java.util.Date
-
-const val MILLISECONDS_IN_SECOND = 1000
-const val SECONDS_IN_MINUTE = 60
-const val SECONDS_IN_HOUR = 3600
-const val MINUTES_IN_HOUR = 60
-const val HOURS_IN_DAY = 24
-const val HOURS_IN_MID_DAY_FORMAT = 12
 
 class TaskViewHolder(itemView: View) : TaskListViewHolder(itemView) {
 
@@ -83,10 +70,10 @@ class TaskViewHolder(itemView: View) : TaskListViewHolder(itemView) {
                 dotProgressBar.visibility = View.VISIBLE
             }
         }
-        date.text = getRelatedDate(item.date)
+        date.text = DateTimeHelper.getFormattedDate(
+            item.date)
     }
 
-    @Suppress("NestedBlockDepth")
     private fun showItemDetails(item: ActionTask) {
         status.visibility = View.VISIBLE
         item.actionInfo.paramsToSubmit?.let { paramsToSubmit ->
@@ -101,28 +88,7 @@ class TaskViewHolder(itemView: View) : TaskListViewHolder(itemView) {
                 val format = relatedParam?.getSafeString("format")
                 // We don't display password fields
                 if (format != ActionParameterEnum.TEXT_PASSWORD.format) {
-                    val stringToAppend = when (type) {
-                        "date" -> {
-                            FormatterUtils.applyFormat("shortDate", entry.value)
-                        }
-                        "time" -> {
-                            entry.value.toString().toDoubleOrNull()?.let { numberOfSeconds ->
-                                val hours: Int = (numberOfSeconds / SECONDS_IN_HOUR).toInt()
-                                val minutes: Int = (numberOfSeconds % SECONDS_IN_HOUR / SECONDS_IN_MINUTE).toInt()
-                                if (format == "duration") {
-                                    "$hours hours $minutes minutes"
-                                } else {
-                                    if (hours >= HOURS_IN_MID_DAY_FORMAT)
-                                        "${hours - HOURS_IN_MID_DAY_FORMAT}:$minutes $PM_KEY"
-                                    else
-                                        "$hours:$minutes $AM_KEY"
-                                }
-                            }
-                        }
-                        else -> {
-                            entry.value.toString()
-                        }
-                    }
+                    val stringToAppend = getFieldOverView(format, type, entry)
                     stringToAppend?.let {
                         if (it.isNotEmpty())
                             sb.append("$stringToAppend , ")
@@ -135,21 +101,19 @@ class TaskViewHolder(itemView: View) : TaskListViewHolder(itemView) {
         }
     }
 
-    private fun getRelatedDate(date: Date): String {
-        val diff: Long = Date().time - date.time
-        val seconds = diff / MILLISECONDS_IN_SECOND
-        val minutes = seconds / SECONDS_IN_MINUTE
-        val hours = minutes / MINUTES_IN_HOUR
-        val days = hours / HOURS_IN_DAY
-
-        return when {
-            days > 0 -> "$days ${getDayWord(days)} ago"
-            hours > 0 -> "$hours ${getHourWord(hours)} ago"
-            minutes > 0 -> "$minutes ${getMinuteWord(minutes)} ago"
-            seconds > 0 -> "$seconds ${getSecondWord(seconds)} ago"
-            seconds == 0L -> "1 ${getSecondWord(1)} ago" // case of click on error server task without
-            // parameters , show always one second ago(instead of 0)
-            else -> ""
+    private fun getFieldOverView(format: String?, type: String?, entry: MutableMap.MutableEntry<String, Any>) : String? {
+        return when (type) {
+            "date" -> {
+                FormatterUtils.applyFormat("shortDate", entry.value)
+            }
+            "time" -> {
+                entry.value.toString().toDoubleOrNull()?.let { numberOfSeconds ->
+                    DateTimeHelper.getFormattedTime(numberOfSeconds, format)
+                }
+            }
+            else -> {
+                entry.value.toString()
+            }
         }
     }
 
---
+++ qmobileui/src/main/java/com/qmobile/qmobileui/action/utils/DateTimeHelper.kt
@@ -0,0 +1,54 @@
+/*
+ * Created by htemanni on 22/6/2022.
+ * 4D SAS
+ * Copyright (c) 2022 htemanni. All rights reserved.
+ */
+
+package com.qmobile.qmobileui.action.utils
+
+import com.qmobile.qmobileui.action.actionparameters.viewholder.AM_KEY
+import com.qmobile.qmobileui.action.actionparameters.viewholder.PM_KEY
+import java.util.Date
+
+const val MILLISECONDS_IN_SECOND = 1000
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
+const val HOURS_IN_MID_DAY_FORMAT = 12
+
+
+object DateTimeHelper {
+
+     fun getFormattedTime(numberOfSeconds: Double, format: String?): String{
+        val hours: Int = (numberOfSeconds / SECONDS_IN_HOUR).toInt()
+        val minutes: Int = (numberOfSeconds % SECONDS_IN_HOUR / SECONDS_IN_MINUTE).toInt()
+        return if (format == "duration") {
+            "$hours hours $minutes minutes"
+        } else {
+            if (hours >= HOURS_IN_MID_DAY_FORMAT)
+                "${hours - HOURS_IN_MID_DAY_FORMAT}:$minutes $PM_KEY"
+            else
+                "$hours:$minutes $AM_KEY"
+        }
+    }
+
+     fun getFormattedDate(date: Date): String {
+        val diff: Long = Date().time - date.time
+        val seconds = diff / MILLISECONDS_IN_SECOND
+        val minutes = seconds / SECONDS_IN_MINUTE
+        val hours = minutes / MINUTES_IN_HOUR
+        val days = hours / HOURS_IN_DAY
+
+        return when {
+            days > 0 -> "$days ${getDayWord(days)} ago"
+            hours > 0 -> "$hours ${getHourWord(hours)} ago"
+            minutes > 0 -> "$minutes ${getMinuteWord(minutes)} ago"
+            seconds > 0 -> "$seconds ${getSecondWord(seconds)} ago"
+            seconds == 0L -> "1 ${getSecondWord(1)} ago" // case of click on error server task without
+            // parameters , show always one second ago(instead of 0)
+            else -> ""
+        }
+    }
+
+}
