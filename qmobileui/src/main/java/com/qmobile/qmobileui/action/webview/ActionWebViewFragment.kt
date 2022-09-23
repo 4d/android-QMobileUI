3b252bf4357aed7d24c4a0e083463d246477b968
htemanni <houssam.temanni@4D.com>
2022-09-23 11:05:59 +0200

    Revert "code improvelements"
    This reverts commit c16387b05502fa5f1080ddc1e645a4d63fa14539.

--- qmobileui/src/main/java/com/qmobile/qmobileui/action/webview/ActionWebViewFragment.kt
+++ qmobileui/src/main/java/com/qmobile/qmobileui/action/webview/ActionWebViewFragment.kt
@@ -65,6 +65,7 @@ class ActionWebViewFragment : BaseFragment() {
             override fun onServerAccessible() {
                 binding.webView.settings.javaScriptEnabled = true
                 binding.webView.addJavascriptInterface(AndroidJavaScriptHandler(requireActivity()), "Android")
+                binding.webView.settings.javaScriptEnabled = true
                 val url = BaseApp.sharedPreferencesHolder.remoteUrl + path
 
                 binding.webView.webViewClient = object : WebViewClient() {
@@ -83,6 +84,9 @@ class ActionWebViewFragment : BaseFragment() {
                         return false
                     }
 
+                    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                        return false
+                    }
 
                     override fun onPageFinished(view: WebView, url: String) {
                         super.onPageFinished(view, url)
