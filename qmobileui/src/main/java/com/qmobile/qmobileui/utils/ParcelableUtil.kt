e0ebf71b94b8834b0c4bf04e1612983c55efebc0
Quentin Marciset <quentin.marciset@4d.com>
2022-12-07 08:58:30 +0100

    utility function for fetching parcelable with newest api

---
+++ qmobileui/src/main/java/com/qmobile/qmobileui/utils/ParcelableUtil.kt
@@ -0,0 +1,44 @@
+/*
+ * Created by qmarciset on 7/12/2022.
+ * 4D SAS
+ * Copyright (c) 2022 qmarciset. All rights reserved.
+ */
+
+package com.qmobile.qmobileui.utils
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
+    else -> {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key) as? T
+    }
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
+    else -> {
+        @Suppress("DEPRECATION")
+        getParcelable(key) as? T
+    }
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelableArrayList(key: String): ArrayList<T>? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableArrayList(key, T::class.java)
+    else -> {
+        @Suppress("DEPRECATION")
+        getParcelableArrayList(key)
+    }
+}
+
+inline fun <reified T : Parcelable> Intent.parcelableArrayList(key: String): ArrayList<T>? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableArrayListExtra(key, T::class.java)
+    else -> {
+        @Suppress("DEPRECATION")
+        getParcelableArrayListExtra(key)
+    }
+}
